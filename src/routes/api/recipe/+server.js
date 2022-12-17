e90c9ce68e9c2b935cc6801de1919d1587abfce6
henryk89 <henrykutrieb@gmail.com>
2022-12-17 03:03:02 -0700

    Forced to have recipe in all prompts

--- src/routes/api/recipe/+server.js
+++ src/routes/api/recipe/+server.js
@@ -14,17 +14,14 @@ import 'dotenv/config'
     const openai = new OpenAIApi(configuration);
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: entered_prompt?.toString(),
+      prompt: `recipe ${entered_prompt?.toString()}`,
       temperature: 0.9,
       max_tokens: 500,
       top_p: 1.0,
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
     });
-    // console.log(response.data)
     return json({
         recipe: response.data.choices[0].text?.substring(2)
  })
 }
-
-//response.data.choices[0].text?.substring(2)
